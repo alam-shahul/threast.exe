2a4ef11cda156587bc762034c5d1a881c04cfa01
Anne Ouyang <anne.ouyang@gmail.com>
2020-04-18 21:15:44 -0400

    Set up router for different views

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,6 +2,10 @@ import React, { useState, useEffect } from "react";
 import { Router } from "@reach/router";
 import NotFound from "./pages/NotFound.js";
 import Homepage from "./pages/Homepage.js";
+import Art from "./pages/Art.js";
+import People from "./pages/People.js";
+import Profile from "./pages/Profile.js";
+import SignIn from "./pages/SignIn.js";
 
 import { firebase } from '@firebase/app';
 import "firebase/auth";
@@ -71,59 +75,22 @@ function App(props) {
 			    userId={userId}
 			    firebaseApp={firebaseApp}
 		    />
+        <Art 
+          path = "/art"
+        />
+        <People 
+          path = "/people"
+        />
+        <Profile 
+          path = "/profile"
+        />
+        <SignIn 
+          path = "/signin"
+        />
 		    <NotFound default />
 		  </Router>
 	  </>
   );
 }
-
-
-// class App extends Component {
-  // makes props available in this component
-  // constructor(props) {
-  //   super(props);
-  //   var firebaseConfig = {
-  //   apiKey: "AIzaSyAvZsDdDGOyMaCHQcXsUDx-85yr9akOhgw",
-  //   authDomain: "threast-website.firebaseapp.com",
-  //   databaseURL: "https://threast-website.firebaseio.com",
-  //   projectId: "threast-website",
-  //   storageBucket: "threast-website.appspot.com",
-  //   messagingSenderId: "933380237825",
-  //   appId: "1:933380237825:web:874189b6c93cdefd"
-  //   };
-  //   this.state = {
-  //   userId: undefined,
-  //   firebaseApp: firebase.initializeApp(firebaseConfig)
-  //   };
-  // }
-
-  // componentDidMount() {
-  //   get("/api/whoami").then((user) => {
-  //   if (user._id) {
-  //   // they are registed in the database, and currently logged in.
-  //   this.setState({ userId: user._id });
-  //   }
-  //   });
-  // }
-
-
-
-//   render() {
-//   return (
-//   <>
-//   <Router>
-//     <Homepage
-//     path="/"
-//     handleLogin={this.handleLogin}
-//     handleLogout={this.handleLogout}
-//     userId={this.state.userId}
-//     firebaseApp={this.state.firebaseApp}
-//     />
-//     <NotFound default />
-//   </Router>
-//   </>
-//   );
-//   }
-// }
 
 export default App;
--- client/src/components/pages/NotFound.js
+++ client/src/components/pages/NotFound.js
@@ -1,18 +1,13 @@
 import React, { Component } from "react";
 
-class NotFound extends Component {
-  constructor(props) {
-    super(props);
-  }
+function NotFound(props) {
 
-  render() {
-    return (
+  return (
       <div>
         <h1>404 Not Found</h1>
         <p>The page you requested couldn't be found.</p>
       </div>
     );
-  }
 }
 
 export default NotFound;
