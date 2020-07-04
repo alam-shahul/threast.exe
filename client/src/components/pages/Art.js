7361488a8988d2bae1bc457c4f8235910ff8a4a3
Shahul Alam <alam.shahul@gmail.com>
2020-07-03 21:51:41 -0500

    Added pagination for art gallery.

--- client/src/components/pages/Art.js
+++ client/src/components/pages/Art.js
@@ -17,7 +17,11 @@ import { get, post } from "../../utilities";
 function Art(props) {
   const url = useLocation().search;
   const parsed = queryString.parse(url);
+  const pageSize = 10;
 
+  const [nextAvailable, setNextAvailable] = useState(false);
+ 
+  const [currentPage, setCurrentPage] = useState(0);
   const [id, setId] = useState(null);
   const [artwork, setArtwork] = useState(null);
   const [gallery, setGallery] = useState(null);
@@ -25,11 +29,74 @@ function Art(props) {
   // TODO: All of this seems a bit hacky... you have basically implemented a state machine. Is that necessary?
   // Perhaps you just need to use the useEffect hook?
 
+  function populateNext() {
+    fetchGallery("next").then((gallerySnapshot) => {
+      if (gallerySnapshot.docs.length > 0) {
+        setGallery(gallerySnapshot.docs);
+        setId(null);
+        setCurrentPage(currentPage+1);
+
+        let lastVisible = gallerySnapshot.docs[gallerySnapshot.docs.length - 1];
+        isNextAvailable(lastVisible);
+      }
+    });
+  }
+
+  function populatePrevious() {
+    fetchGallery("previous").then((gallerySnapshot) => {
+      if (gallerySnapshot.docs.length > 0) {
+        setGallery(gallerySnapshot.docs);
+        setId(null);
+        setCurrentPage(currentPage-1);
+
+        setNextAvailable(true);
+      }
+    });
+  }
+
+  function isNextAvailable(lastVisible) {
+    let query = (props.user) ?
+      firestore.collection("art").orderBy("lastUpdated", "desc")
+      :     
+      firestore.collection("art").orderBy("lastUpdated", "desc").where("visibility", "==", "public");
+    
+    query
+      .startAfter(lastVisible)
+      .get().then((gallerySnapshot) => {
+        let nextStatus = (gallerySnapshot.docs.length > 0);
+        setNextAvailable(nextStatus);
+      });
+  }
+
+  function fetchGallery(type = "initial") {
+    let query = (props.user) ?
+      firestore.collection("art").orderBy("lastUpdated", "desc")
+      :     
+      firestore.collection("art").orderBy("lastUpdated", "desc").where("visibility", "==", "public");
+
+    if (type === "initial") {
+      query = query.limit(pageSize);
+    }
+    else if (type === "previous") {
+      let firstVisible = gallery[0];
+      query = query
+        .endBefore(firstVisible)
+        .limitToLast(pageSize);
+    }
+    else if (type === "next") {
+      let lastVisible = gallery[gallery.length-1];
+      query = query
+        .startAfter(lastVisible)
+        .limit(pageSize);
+    }
+    
+    return query.get()
+  }
+
   if (parsed.id) {
     if(!artwork || (parsed.id != id)) {
       firestore.collection("art").doc(parsed.id).get()
         .then((artSnapshot) => {
-          //console.log(artSnapshot.data());
           setArtwork(artSnapshot.data());
           setId(parsed.id);
         });
@@ -39,24 +106,32 @@ function Art(props) {
     if(!gallery || id) {
       console.log(gallery);
       console.log(props.user);
-     
-      let query = (props.user) ?
-        firestore.collection("art").orderBy("lastUpdated")
-        :     
-        firestore.collection("art").orderBy("lastUpdated").where("visibility", "==", "public");
-
-      query.get()
-        .then((gallerySnapshot) => {
-          setGallery(gallerySnapshot.docs.reverse());
-          setId(null);
-        });
+    
+      fetchGallery().then((gallerySnapshot) => {
+        isNextAvailable(gallerySnapshot.docs[gallerySnapshot.docs.length - 1]);
+        setGallery(gallerySnapshot.docs);
+        setId(null);
+        setCurrentPage(0);
+      });
     }
   }
   return (
     <>
       { id ?
         (artwork ? <Artwork artwork={artwork} id={id} user={props.user}/> : <div>Loading...</div>):
-        <Gallery gallery={gallery} title={"3E Community Art"}/> 
+        <>
+          <Gallery gallery={gallery} title={"3E Community Art"}/>
+          { (currentPage != 0) ?
+            <button onClick={e => populatePrevious()}>Previous Page</button>
+            :
+            <></>
+          }
+          { (nextAvailable) ?
+            <button onClick={e => populateNext()}>Next Page</button>
+            :
+            <></>
+          }
+        </> 
       }     
     </>
   );
